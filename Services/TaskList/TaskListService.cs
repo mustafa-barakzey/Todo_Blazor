c706da04240be11f432abbc21242b0f2462d6093
mustafa-barakzey <mustafabarakzey@gmail.com>
2024-01-15 07:00:44 -0800

    add task

--- Services/TaskList/TaskListService.cs
+++ Services/TaskList/TaskListService.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http.Json;
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
 using Todo_Blazor.Services.TaskList.ViewModels;
 using Todo_Blazor.ViewModel;
 
@@ -18,6 +21,18 @@ namespace Todo_Blazor.Services.TaskList
             var result = await _httpClient.GetFromJsonAsync<OperationResult<GetTaskResult>>($"Task/Get?ListId={listId}");
 
             return result?.Data ?? new();
+        }
+
+        public async Task AddTaskAsync(long listId,string taskTitle)
+        {
+            var result = await _httpClient.PostAsync($"Task/Add", new StringContent(
+            JsonConvert.SerializeObject(new { listId,title=taskTitle}),
+            Encoding.UTF8,
+            "application/json"));
+
+            if (result.StatusCode != HttpStatusCode.Created)
+                throw new Exception(await result.Content.ReadAsStringAsync());
+            return;
         }
     }
 }
